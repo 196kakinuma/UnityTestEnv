d896aa692eecbbfbb9fd963e62633fe3297078dd
196kakinuma <196thinline@gmail.com>
2018-12-12 13:38:35 +0900

    Initial commit

---
+++ Assets/RotateCtrl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCtrl : MonoBehaviour {
+
+
+    GameObject manipulateObj = null;
+    bool isManipulating = false;
+    Vector3 startDirection = Vector3.zero;
+    GameObject parentObj;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var trackedObject = GetComponent<SteamVR_TrackedObject>();
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            if(manipulateObj!=null)
+            {
+                parentObj = new GameObject();
+                parentObj.transform.position = manipulateObj.transform.position;
+                parentObj.transform.LookAt(this.transform.position);
+                manipulateObj.transform.parent = parentObj.transform;
+            }
+        }
+        if (device.GetPress(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            //Debug.Log("トリガーを深く引いている");
+            if (manipulateObj != null)
+            {
+                isManipulating = true;
+                //移動した分だけ回転させる
+                parentObj.transform.LookAt(this.transform.position);
+
+            }
+        }
+        if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            manipulateObj.transform.parent = null;
+            var a = parentObj;
+            parentObj = null;
+            Destroy(a);
+
+            isManipulating = false;
+            manipulateObj = null;
+
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (manipulateObj != null) return;
+
+        if (other.gameObject.tag == "RotateObject")
+        {
+            manipulateObj = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (isManipulating) return;
+        if (other.gameObject.tag != "RotateObject") return;
+        manipulateObj = null;
+
+    }
+}
---
+++ Assets/fj.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fj : MonoBehaviour
+{
+    SteamVR_TrackedController trackedController;
+    GameObject grababbleObject;
+    FixedJoint joint;
+
+    void Start()
+    {
+        trackedController = gameObject.GetComponent<SteamVR_TrackedController>();
+
+        if (trackedController == null)
+        {
+            trackedController = gameObject.AddComponent<SteamVR_TrackedController>();
+        }
+
+        trackedController.TriggerClicked += new ClickedEventHandler(DoTriggerClicked);
+        trackedController.TriggerUnclicked += new ClickedEventHandler(DoTriggerUnclicked);
+
+        joint = gameObject.GetComponent<FixedJoint>();
+    }
+
+    public void DoTriggerClicked(object sender, ClickedEventArgs e)
+    {
+        grab();
+    }
+
+    public void DoTriggerUnclicked(object sender, ClickedEventArgs e)
+    {
+        release();
+    }
+
+    void grab()
+    {
+        if (grababbleObject == null || joint.connectedBody != null)
+        {
+            return;
+        }
+
+        joint.connectedBody = grababbleObject.GetComponent<Rigidbody>();
+    }
+    void release()
+    {
+        if (joint.connectedBody == null)
+        {
+            return;
+        }
+
+        Rigidbody rigidBody = joint.connectedBody;
+        joint.connectedBody = null;
+
+        var device = SteamVR_Controller.Input((int)trackedController.GetComponent<SteamVR_TrackedObject>().index);
+        rigidBody.velocity = device.velocity;
+        rigidBody.angularVelocity = device.angularVelocity;
+        rigidBody.maxAngularVelocity = rigidBody.angularVelocity.magnitude;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        grababbleObject = other.gameObject;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        grababbleObject = null;
+    }
+}
